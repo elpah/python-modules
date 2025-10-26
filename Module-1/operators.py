2ae5458944e49ee91f6ca27963df56e0c9b9c5db
Elpah <obengelpachris@gmail.com>
2025-10-26 03:51:07 +0100

    operators

---
+++ Module-1/operators.py
@@ -0,0 +1,52 @@
+# Arithmetic Operators
+
+num1 = 4
+num2 = 2
+# Addition
+sum_result = num1 + num2
+
+# Subtraction 
+difference = num1 - num2
+
+# Division
+division_result = num1 / num2
+
+# Multiplication
+product = num1 * num2
+
+#Modulus
+remainder = num1 % num2
+
+#Exponentiation - num1 raise to the power num2
+exponent_result = num1 **num2 
+
+#Floor Division - divide and round up result to the nearest whole number
+#example 5//2 = 3. so ideally is 2.5, nearing whole numnber is 3.
+
+print("Sum of num1 and num2 is: ",sum_result)
+print("Difference of num1 and num2 is: ",difference)
+print("Division of num1 and num2 is: ",division_result)
+print("Product of num1 and num2 is: ",product)
+print("Remainder of num1 and num2 is: ",remainder)
+print( "Exponent result of num1 and num2 is: ", exponent_result)
+
+
+
+# Logical Operators, and comparison operators. They kind of work handin hand.
+# Logical Operators. and, or, not
+#Comparison operators; >, <, ==, !=, >=, <= 
+#usage example
+
+print((num1 > num2) and (num1 < 5)) #- this will return a boolean.
+
+print(num1 == 4)
+print(num1 != 4)
+print(num1 >= 4)
+print(num1 <= 4)
+
+print(not(num1>num2))
+
+
+
+
+	
