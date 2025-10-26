f5c2f2efb7795cbe28806e028e11296e6e2bd715
elpah <obengelpachris@gmail.com>
2025-10-26 08:42:39 +0100

    functions in py

---
+++ Module-2/functions.py
@@ -0,0 +1,15 @@
+# A A function is a block of code that performs a specific task. You can call it multiple times throughout your program.
+# and pass in different values to get different results.
+
+def function_name(params):
+    print("Function called with params:", params)
+
+function_name("Hello, World!")
+function_name(42)
+
+def add(a, b):
+    return a+b
+
+result = add(5, 7)
+print("The sum is:", result)
+
