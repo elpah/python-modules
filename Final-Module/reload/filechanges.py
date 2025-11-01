af8c5f7d86394381a7b86f2d35318dba50416c3f
elpah <obengelpachris@gmail.com>
2025-11-01 23:05:51 +0100

    reload in python

---
+++ Final-Module/reload/filechanges.py
@@ -0,0 +1,8 @@
+import os
+
+
+def printchanges():
+    print("Current directory contents:")
+    contents = os.listdir(r'/Users/elpah/Desktop/python_modules/Final-Module/reload/')
+    print(contents)
+
