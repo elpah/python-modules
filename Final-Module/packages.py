bc1c753a908e18fd953b71eb281e56369701f1b1
elpah <obengelpachris@gmail.com>
2025-11-01 23:28:01 +0100

    explored python packages

---
+++ Final-Module/packages.py
@@ -0,0 +1,51 @@
+# A package is a collection of modules and both library and framework are often used interchangeably with packages. 
+# Libraries can also be a collection of packages with specific purpose, whereas the term framework 
+# is usually used where certain flow and architecture is involved. 
+
+
+
+# Data analysis and Data Science popular packages
+# Numpy, Scipy, Matplotlib, Scikit-Learn
+
+# Pandas - Python Data analysis 
+# primary for working with datasets, and provides functions for cleaning, analysing and manipulating data
+
+# compare different cols and find arithmetic mean , max and min values.
+# primart DS are series and data frames
+
+# Series - Single dimensional and can be compared to a colum in a table
+# Data frames- Multi dimensional and can store data tables efficiently
+
+# Most common panda apps
+# Reading CSV files, and Json objects
+
+# the library
+# import pandas as pd
+
+#Numpy- Numerical python
+# scientist use for signal, image, stats and quantum
+
+
+
+#Machine Learning and AI
+#PyTorch
+#TensorFlow
+
+
+#Web Frame Works
+# Fullstack frameworks - Form generations, template layoyts, HTTP request handling, WSGI Interfaces, DB connection handling
+# most popular: Django, web2py, pyramid
+
+#Microframeworks - Microframeworks are a lighter version of fullstacks that do not offer as many patterns 
+# and functionalities. They are usually used in smaller web projects and building APIs. Flask, Bottle, Dash, 
+# and CherryPy are some of the popular microframeworks.
+
+#Asynchronous -types are used to handle a large sets of concurrent connections.
+#  They are mainly built using Async IO networking libraries. Growler, AIOHTTP,
+#  and Sanic, are some of the names you'll encounter. Choosing a framework can depend on many factors. 
+
+#Django
+# Django is a high level framework that encourages clean design and rapid development. It's a full-stack framework that's rich in features and libraries. 
+# It's secure and has templating systems and third party supports. 
+
+#Flask - is a microframework and better used for smaller projects. It's easy to learn, simple to use, and as a large library of add-ons.
