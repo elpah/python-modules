b994d495e1b8e388982f3741f3afe09b84c03ab2
elpah <obengelpachris@gmail.com>
2025-11-01 19:30:03 +0100

    module 3 completed

---
+++ Module-3/abstract_classes_methods.py
@@ -0,0 +1,21 @@
+import abc from ABC, abstractmethod
+
+class Employee(ABC):
+    @abstractmethod
+    def donate(self):
+        pass
+
+class Donation(Employee):
+    def donate(self):
+        a = input("How much would you like to donate?")
+
+amounts = []
+john = Donation()
+j = john.donate()
+amounts.append(j)
+
+peter = Donation()
+p = peter.donate()
+amounts.append(p)
+
+print(amounts)
