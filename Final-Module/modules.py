7d6e5017accc42a8a17d0cc059fc58ce8e90f83f
elpah <obengelpachris@gmail.com>
2025-11-02 15:16:14 +0100

    Final Module Completedgit status

--- Final-Module/modules.py
+++ Final-Module/modules.py
@@ -17,12 +17,23 @@ print(leapdays)
 is_it_leap = calendar.isleap(2024)
 print(is_it_leap)
 
-
-import json
-
-
- #User defined modules 
-sys.path.insert(1, r'/Users/elpah/Desktop/python_modules/Final-Module/sample_to_import.py')
+#User defined modules 
+# sys.path.insert(1, r'/Users/elpah/Desktop/python_modules/Final-Module/sample_to_import.py') - add the directory path if you are importing from another directory
 import sample_to_import
 
 print(sample_to_import.names)
+
+#instead of importing the whole math module, we can specifically import what we need. 
+#example
+# import math
+# import math as variable_name - exampl: import math as m
+# we can also import multiple functions that we need from math. example : from math import sqrt, factorial, log10
+
+
+from math import sqrt
+print("importing maths module")
+# sqroot = math.sqrt(25)
+sqroot = sqrt(25)
+print(sqroot)
+
+
